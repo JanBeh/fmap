9888c07ff75810956b11fc026da10ad438d2864f
Jan Behrens <jbe@magnetkern.de>
2023-06-12 04:51:32 +0200

    Added Send and !Send impls for Iterator and Future
    Implementations for boxed iterators that are `Send` and boxed futures
    that are `!Send` have been added.

--- src/impls/future.rs
+++ src/impls/future.rs
@@ -6,7 +6,14 @@ use std::future::Future;
 use std::pin::Pin;
 
 impl<'a, A> FunctorSelf<'a>
-    for Pin<Box<dyn 'a + Send + Future<Output = A>>>
+    for Pin<Box<dyn 'a + Future<Output = A>>>
+where
+    A: 'a,
+{
+    type FmapInOut = A;
+}
+impl<'a, A> FunctorSelf<'a>
+    for Pin<Box<dyn 'a + Future<Output = A> + Send>>
 where
     A: 'a,
 {
@@ -14,11 +21,29 @@ where
 }
 
 impl<'a, A, B> Functor<'a, B>
-    for Pin<Box<dyn 'a + Send + Future<Output = A>>>
+    for Pin<Box<dyn 'a + Future<Output = A>>>
 where
     A: 'a,
 {
-    type Mapped<'b> = Pin<Box<dyn 'b + Send + Future<Output = B>>>
+    type Mapped<'b> = Pin<Box<dyn 'b + Future<Output = B>>>
+    where
+        'a: 'b,
+        B: 'b;
+    fn fmap<'b, F>(self, mut f: F) -> Self::Mapped<'b>
+    where
+        'a: 'b,
+        B: 'b,
+        F: 'b + Send + FnMut(Self::FmapIn) -> B,
+    {
+        Box::pin(async move { f(self.await) })
+    }
+}
+impl<'a, A, B> Functor<'a, B>
+    for Pin<Box<dyn 'a + Future<Output = A> + Send>>
+where
+    A: 'a,
+{
+    type Mapped<'b> = Pin<Box<dyn 'b + Future<Output = B> + Send>>
     where
         'a: 'b,
         B: 'b;
@@ -33,7 +58,23 @@ where
 }
 
 impl<'a, A> FunctorMut<'a>
-    for Pin<Box<dyn 'a + Send + Future<Output = A>>>
+    for Pin<Box<dyn 'a + Future<Output = A>>>
+where
+    A: 'a,
+{
+    fn fmap_mut<F>(&mut self, f: F)
+    where
+        F: 'a + Send + FnMut(&mut Self::FmapInOut),
+    {
+        let this = std::mem::replace(
+            self,
+            Box::pin(async move { panic!("poisoned FunctorMut") }),
+        );
+        *self = this.fmap_fn_mutref(f);
+    }
+}
+impl<'a, A> FunctorMut<'a>
+    for Pin<Box<dyn 'a + Future<Output = A> + Send>>
 where
     A: 'a,
 {
@@ -50,7 +91,20 @@ where
 }
 
 impl<'a, A, B> Pure<'a, B>
-    for Pin<Box<dyn 'a + Send + Future<Output = A>>>
+    for Pin<Box<dyn 'a + Future<Output = A>>>
+where
+    A: 'a,
+{
+    fn pure<'b>(b: B) -> Self::Mapped<'b>
+    where
+        'a: 'b,
+        B: 'b,
+    {
+        Box::pin(std::future::ready(b))
+    }
+}
+impl<'a, A, B> Pure<'a, B>
+    for Pin<Box<dyn 'a + Future<Output = A> + Send>>
 where
     A: 'a,
     B: Send,
@@ -65,7 +119,21 @@ where
 }
 
 impl<'a, A, B> Monad<'a, B>
-    for Pin<Box<dyn 'a + Send + Future<Output = A>>>
+    for Pin<Box<dyn 'a + Future<Output = A>>>
+where
+    A: 'a,
+{
+    fn bind<'b, F>(self, mut f: F) -> Self::Mapped<'b>
+    where
+        'a: 'b,
+        B: 'b,
+        F: 'b + Send + FnMut(Self::FmapIn) -> Self::Mapped<'b>,
+    {
+        Box::pin(async move { f(self.await).await })
+    }
+}
+impl<'a, A, B> Monad<'a, B>
+    for Pin<Box<dyn 'a + Future<Output = A> + Send>>
 where
     A: Send + 'a,
     B: Send,
